6f8fb18c520ac2536169423036c2c0ed169bd3c0
sivaharani <sivaharinishanthi@gmail.com>
2025-07-11 15:06:16 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+  const message = document.getElementById("message");
+
+  if (username === "admin" && password === "1234") {
+    message.style.color = "green";
+    message.textContent = "Login successful!";
+  } else {
+    message.style.color = "red";
+    message.textContent = "Invalid username or password.";
+  }
+});
